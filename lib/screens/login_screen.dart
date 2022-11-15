6b53a88d4c7adb5714fc8eeaaee5ad9275e9d19b
Andres Molina <andresitomolinaclan@gmail.com>
2022-11-15 10:44:47 +0100

    Vista del formulario completada

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:trabajo_cop_flutter/widgets/widgets.dart';
+
+import '../ui/input_decorations.dart';
 
 class LoginScreen extends StatelessWidget{
   @override
@@ -17,13 +18,75 @@ class LoginScreen extends StatelessWidget{
                     SizedBox(height: 10),
                     Text('Login', style: Theme.of(context).textTheme.headline4),
                     SizedBox(height: 30),
+
                   ],
                 )
               ),
+
+              SizedBox( height: 50),
+              Text('Crear una nueva cuenta', style: TextStyle(fontSize:18)),
+              
+              SizedBox( height: 50),
+
             ],
           ),
         )
       )
+    );
+  }
+}
+
+class _LoginForn extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'prueba@gmail.com',
+                labelText: 'Correo electronico',
+                prefixIcon: Icons.alternate_email_rounded
+              ),
+            ),
+
+            SizedBox(height:30),
+
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '*******',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline),
+            ),
+            
+            SizedBox(height:30),
+            
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical:15),
+                child:Text(
+                  'Ingresar',
+                  style: TextStyle( color: Colors.white),
+                )
+              ),
+              onPressed: (){
+
+              },
+            )
+          ],
+        )
+      ),
     );
   }
 }
