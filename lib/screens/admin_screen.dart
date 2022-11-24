caee364eb7c2b878e28d0af82b9654fb9274f247
Ana <blasco.paana21@cadiz.salesianos.edu>
2022-11-24 09:11:43 +0100

    Cambios admin

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -6,62 +6,47 @@ import '../services/services.dart';
 class AdminScreen extends StatelessWidget {
   AdminScreen({Key? key}) : super(key: key);
   List<DataUser> users = [];
-  
 
   //void deleteUser()async{
-   //         await http.delete(Uri.parse("http://10.0.2.2:1337/apis/${widget.users.id}"));
-   //        Navigator.of(context).pushAndRemoveUntil(
-   //             MaterialPageRoute(builder: (BuildContext context) => DisplayUsers()),
-    //            (Route<dynamic> route) => false);
-    //      }
+  //         await http.delete(Uri.parse("http://10.0.2.2:1337/apis/${widget.users.id}"));
+  //        Navigator.of(context).pushAndRemoveUntil(
+  //             MaterialPageRoute(builder: (BuildContext context) => DisplayUsers()),
+  //            (Route<dynamic> route) => false);
+  //      }
 
   @override
   Widget build(BuildContext context) {
-        
-        final userListService = Provider.of<UsersListService>(context);
-        users = userListService.users.cast<DataUser>();
+    final userListService = Provider.of<UsersListService>(context);
+    users = userListService.users.cast<DataUser>();
 
-        return MaterialApp(
-          home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Lista de usuarios registrados'),
-            centerTitle: true,
-          ),
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de usuarios registrados'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
 
-          body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            final user = users[index];
-            
-
-            return ListTile(
-              title: Text(user.name),
-              subtitle: Text(user.email),
-              trailing: 
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () => {Navigator.pushReplacementNamed(context, 'edit')},
-                ), 
-                
-                // trailing: IconButton(
-                //     icon: const Icon(Icons.delete),
-                //     onPressed: () => {Navigator.pushReplacementNamed(context, 'admin')},
-                    
-                //   ),
-                  
-              );
-            },
+          return ListTile(
+            title: Text(user.name),
+            subtitle: Text(user.email),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () =>
+                  {Navigator.pushReplacementNamed(context, 'admin')},
             ),
-          )
-        );
 
+            // trailing: IconButton(
+            //     icon: const Icon(Icons.delete),
+            //     onPressed: () => {Navigator.pushReplacementNamed(context, 'admin')},
+
+            //   ),
+          );
+        },
+      ),
+    ));
   }
-}
-
-abstract class ListItem {
-  /// The title line to show in a list item.
-  Widget buildTitle(BuildContext context);
-
-  /// The subtitle line, if any, to show in a list item.
-  Widget buildSubtitle(BuildContext context);
 }
