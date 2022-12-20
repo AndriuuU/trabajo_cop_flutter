cd630e52faf14c7a8361d35e9cf95e4c0bbe918f
AnaBlascoParra <ana.21guns@gmail.com>
2022-12-20 09:01:06 +0100

    models2

--- lib/services/articulos_service.dart
+++ lib/services/articulos_service.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/articles_models.dart';
+import '../models/articles_response.dart';
+
 class articulosService extends ChangeNotifier {
   final String _baseUrl = 'semillero.allsites.es';
 
@@ -10,7 +13,7 @@ class articulosService extends ChangeNotifier {
     this.getListArticulos();
   }
 
-  List<Articulos> listArticulos = [];
+  List<Articles> listArticulos = [];
 
   getListArticulos() async {
     final url = Uri.http(_baseUrl, '/public/api/cicles');
