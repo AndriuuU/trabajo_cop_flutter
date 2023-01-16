8e3885b44d32171c97436cd3a97701fad4ef57e2
AnaBlascoParra <ana.21guns@gmail.com>
2023-01-16 13:36:19 +0100

    nueva screen home (welcome screen)

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,4 +2,4 @@ export 'package:trabajo_cop_flutter/screens/home_screen.dart';
 export 'package:trabajo_cop_flutter/screens/register_screen.dart';
 export 'package:trabajo_cop_flutter/screens/admin_screen.dart';
 export 'package:trabajo_cop_flutter/screens/login_screen.dart';
-
+export 'package:trabajo_cop_flutter/screens/welcome_screen.dart';
